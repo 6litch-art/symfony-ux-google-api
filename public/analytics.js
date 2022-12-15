180530cfd2711e6e3defe414738d7ad1938427de
Marco Meyer (www) <marco.meyerconde@gmail.com>
2022-12-16 00:01:17 +0100

    Update

--- public/analytics.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*****************************!*\
-  !*** ./assets/analytics.js ***!
-  \*****************************/
-window.addEventListener('load', function (event) {});
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYW5hbHl0aWNzLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQ0FBLE1BQU0sQ0FBQ0MsZ0JBQWdCLENBQUMsTUFBTSxFQUFFLFVBQVNDLEtBQUssRUFBRSxDQUVoRCxDQUFDLENBQUMsQyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2Fzc2V0cy9hbmFseXRpY3MuanMiXSwic291cmNlc0NvbnRlbnQiOlsiXG53aW5kb3cuYWRkRXZlbnRMaXN0ZW5lcignbG9hZCcsIGZ1bmN0aW9uKGV2ZW50KSB7XG4gICAgXG59KTtcbiJdLCJuYW1lcyI6WyJ3aW5kb3ciLCJhZGRFdmVudExpc3RlbmVyIiwiZXZlbnQiXSwic291cmNlUm9vdCI6IiJ9
