ebf1c04ce54a09fd8eb4d912c8284a38aceb3111
Marco Meyer <marco.meyer@glitchr.io>
2026-06-10 02:52:36 +0200

    upgrade to SF8.0

--- src/Controller/GaController.php
+++ src/Controller/GaController.php
@@ -5,7 +5,7 @@ namespace Google\Controller;
 use Google\Service\GaService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class GaController extends AbstractController
 {
--- src/Controller/GrController.php
+++ src/Controller/GrController.php
@@ -7,7 +7,7 @@ use Google\Form\GrFormTypeV3;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class GrController extends AbstractController
 {
